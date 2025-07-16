f102b1649e7e955c8cb51c6e5ac9f38e307c7ad1
Jaiprakash Kushwaha <kushwaha02071999@gmail.com>
2025-07-16 16:37:57 +0530

    dyanmic table

---
+++ src/table/index.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import "./styles.module.css";
+
+// Example: 20 supplier rows shaped for DynamicList
+
+const ListComponent = ({ suppliersData = [], columns = [] }) => {
+  return (
+        <table>
+          <thead>
+            {columns.length > 0 &&
+              columns.map((column) => (
+                <th key={column.key} style={{ width: column.width }}>
+                  {column.label}
+                </th>
+              ))}
+          </thead>
+          <tbody>
+            {suppliersData.map((supplier) => (
+              <tr key={supplier.id}>
+                {columns.map((column) => (
+                  <td key={column.key} style={{ width: column.width }}>
+                    {supplier[column.key] || "-"}
+                  </td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+  );
+};
+
+export default ListComponent;
